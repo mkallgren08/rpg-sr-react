fb2908f0ec649c523ea9538427aa04804d1fdb52
mkallgren08 <mkallgren08@gmail.com>
2019-04-10 23:31:33 -0400

    Building for production

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -2,5 +2,5 @@ export const AUTH_CONFIG = {
   domain: 'mkallgren.auth0.com',
   clientId: '4JgpEi3xeChbGhFIFEpY0taJFs2qQgIE',
   callbackUrlDEV: 'http://localhost:3000/callback',
-  callbackUrlPROD: 'https://www.sr-characters.herokuapp.com/callback'
+  callbackUrlPROD: 'https://sr-characters.herokuapp.com/callback'
 }
